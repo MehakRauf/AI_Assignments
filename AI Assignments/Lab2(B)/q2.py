65c5dd963486c139ccf70b09014c273437cade25
MEHAK FATIMA <139680303+MehakRauf@users.noreply.github.com>
2024-02-16 19:36:43 +0500

    Add files via upload

---
+++ AI Assignments/Lab2(B)/q2.py
@@ -0,0 +1,22 @@
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+
+def classify_bmi(bmi):
+    if bmi < 18.4:
+        return "Underweight"
+    elif bmi < 24.9:
+        return "Normal weight"
+    elif bmi < 25.0:
+        return "Overweight"
+    else:
+        return "Obese"
+
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
+# 1.6764
+bmi = calculate_bmi(weight, height)
+classification = classify_bmi(bmi)
+
+print("Your BMI is:", bmi)
+print("Classification:", classification)
