1c71402de49ccdf27d84f7e136bc26d7fc3c23a4
MEHAK FATIMA <139680303+MehakRauf@users.noreply.github.com>
2024-02-18 16:28:05 +0500

    Add files via upload

---
+++ AI Assignments/Lab3/Exercise 3/random_module.py
@@ -0,0 +1,9 @@
+import random;
+
+list=[1,2,5,6,4,9,0]
+random.shuffle(list)
+print(list)
+print(random.randint(1,3))
+print(random.randrange(2,5))
+print(random.random())
+print(random.uniform(0.1,0.9))
